0d8d44069147250ccf750d27c1b269ef4fa3d30a
Louai Al ameiri <Louai199947@gmail.com>
2026-06-23 12:38:19 +0400

    Added pilot  classes

--- Flight Management System/Models/Pilot.cs
+++ Flight Management System/Models/Pilot.cs
@@ -6,5 +6,13 @@ namespace Flight_Management_System.Models
 {
     internal class Pilot
     {
+    public int pilotId { get; set; }
+    public string pilotName { get; set; }
+    public string pilotPhone { get; set; }
+    public string licenseNumber { get; set; }
+    public int flightHours { get; set; }
+    public bool isAvailable { get; set; }
+        
+
     }
 }
