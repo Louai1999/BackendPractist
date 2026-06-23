2222fd175e7dea09cf95e909df8316999ea24737
Louai Al ameiri <Louai199947@gmail.com>
2026-06-23 12:34:06 +0400

    Added passenger classes

---
+++ Flight Management System/Models/Passenger.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Flight_Management_System.Models
+{
+    internal class Passenger
+    {
+        public int passengerId {  get; set; }
+        public string passengerName { get; set; }
+        public string passengerEmail { get; set; }
+        public string passengerPhone { get; set; }
+        public string passportNumber { get; set; }
+        public string nationality { get; set; }
+
+    }
+}
