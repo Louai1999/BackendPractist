df9b62897ba1e18bf8b0e237c31d44bce82b1cde
Louai Al ameiri <Louai199947@gmail.com>
2026-06-23 15:27:23 +0400

    Added booking classes

--- Flight Management System/Models/Booking.cs
+++ Flight Management System/Models/Booking.cs
@@ -6,5 +6,13 @@ namespace Flight_Management_System.Models
 {
     internal class Booking
     {
+    public int bookingId {  get; set; }
+    public int passengerId { get; set; }
+    public int flightId { get; set; }
+    public string seatNumber { get; set; }
+    public string bookingDate { get; set; }
+    public decimal totalPrice { get; set; }
+    public string status { get; set; }
+
     }
 }
