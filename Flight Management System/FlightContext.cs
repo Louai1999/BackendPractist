bb431d5a726c9c94d93b89bd56ab42e409fccacb
Louai Al ameiri <Louai199947@gmail.com>
2026-06-23 15:52:20 +0400

    Adde flightContext classes

--- Flight Management System/FlightContext.cs
+++ Flight Management System/FlightContext.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Flight_Management_System.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +7,11 @@ namespace Flight_Management_System
 {
     internal class FlightContext
     {
+    public List<Passenger>  passengers {  get; set; }
+    public List<Pilot> pilots { get; set; }
+    public List<Aircraft> aircrafts { get; set; }
+    public List<Flight> flights { get; set; }
+    public List<Booking> bookings { get; set; }
+    
     }
 }
