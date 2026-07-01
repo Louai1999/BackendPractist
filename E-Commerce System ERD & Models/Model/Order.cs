3a64fb6e0323154da2cfc31f8ac9aa273e36c06a
Louai Al ameiri <Louai199947@gmail.com>
2026-07-01 16:19:00 +0400

    Order Constraints added

--- E-Commerce System ERD & Models/Model/Order.cs
+++ E-Commerce System ERD & Models/Model/Order.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace E_Commerce_System_ERD___Models.Model
@@ -7,12 +9,26 @@ namespace E_Commerce_System_ERD___Models.Model
     public class Order
     {
 
+
+        [Key,Required , DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int orderId { get; set; }//System generated 
+
+        [ForeignKey("userId"),Required]
         public int userId { get; set; } // Foreign key
+
+        [Required]
         public DateTime orderDate { get; set; } // Calculated 
+
+        [Required, Range(0,double.MaxValue)]
         public decimal totalAmount { get; set; } // Calculated 
-        public string status { get; set; } //Defult value 
+
+        [Required, MaxLength(30)]
+        public string status { get; set; } = "Pending"; //Defult value 
+
+        [Required, MaxLength(300)]
         public string shippingAddress { get; set; } // User input 
+
+        [Required,MaxLength(50)]
         public string paymentMethod { get; set; }// Calculated 
     }
 }
