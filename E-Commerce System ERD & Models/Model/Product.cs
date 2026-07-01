bd819d69ab5fd854902597314f9826fc1213d9a1
Louai Al ameiri <Louai199947@gmail.com>
2026-07-01 16:11:09 +0400

    Producat Constraints

--- E-Commerce System ERD & Models/Model/Product.cs
+++ E-Commerce System ERD & Models/Model/Product.cs
@@ -1,20 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace E_Commerce_System_ERD___Models.Model
 {
     public class Product
     {
+
+        [Key ,DatabaseGenerated(DatabaseGeneratedOption .Identity),Required]
         public int productId { get; set; } // System generated 
+
+
+        [Required , MaxLength(150)]
         public string productName { get; set; } // User input 
-        public string description { get; set; } // User input 
+
+
+        [MaxLength(1000)]
+        public string? description { get; set; } // User input 
+
+
+        [Required, Range(0, double.MaxValue)]
         public decimal price { get; set; } // Calculated 
-        public int stockQuantity { get; set; } // Calculated 
-        public string imageUrl { get; set; } // User input
+
+
+        [Required, Range(0, int.MaxValue)]
+        public int stockQuantity { get; set; } = 0; // Calculated 
+
+        [MaxLength(300)]
+        public string? imageUrl { get; set; } // User input
+
+        [ForeignKey("categoryId"),Required]
         public int categoryId { get; set; } // Foreign key 
+
+        [Required]
         public DateTime createdAt { get; set; } // Calcilated
-        public bool isAvailable { get; set; } // Defualt value 
+
+
+        public bool isAvailable { get; set; } = true; // Defualt value 
 
 
     }
