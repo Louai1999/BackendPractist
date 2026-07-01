e2fdb03b47283f669d2b620694ac632bf5fa9248
Louai Al ameiri <Louai199947@gmail.com>
2026-07-01 15:26:47 +0400

    Order class comment done

--- E-Commerce System ERD & Models/Model/Order.cs
+++ E-Commerce System ERD & Models/Model/Order.cs
@@ -7,12 +7,12 @@ namespace E_Commerce_System_ERD___Models.Model
     public class Order
     {
 
-        public int orderId { get; set; }
-        public int userId { get; set; }
-        public DateTime orderDate { get; set; }
-        public decimal totalAmount { get; set; }
-        public string status { get; set; }
-        public string shippingAddress { get; set; } 
-        public string paymentMethod { get; set; }
+        public int orderId { get; set; }//System generated 
+        public int userId { get; set; } // Foreign key
+        public DateTime orderDate { get; set; } // Calculated 
+        public decimal totalAmount { get; set; } // Calculated 
+        public string status { get; set; } //Defult value 
+        public string shippingAddress { get; set; } // User input 
+        public string paymentMethod { get; set; }// Calculated 
     }
 }
--- E-Commerce System ERD & Models/Model/Review.cs
+++ E-Commerce System ERD & Models/Model/Review.cs
@@ -6,12 +6,12 @@ namespace E_Commerce_System_ERD___Models.Model
 {
     public class Review
     {
-        public int reviewId { get; set; }
-        public int userId { get; set; }
-        public int productId { get; set; }
-        public int rating { get; set; }
-        public string comment { get; set; }
-        public DateTime reviewDate { get; set; }
+        public int reviewId { get; set; } // System generated 
+        public int userId { get; set; } // Foreign key 
+        public int productId { get; set; } // Foreign Key 
+        public int rating { get; set; } // Calculated 
+        public string comment { get; set; } // User input 
+        public DateTime reviewDate { get; set; } // Calculated
 
 
     }
