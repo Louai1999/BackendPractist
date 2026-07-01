126f1d60a1ec942060c9a3f5a1b9982e7c603653
Louai Al ameiri <Louai199947@gmail.com>
2026-07-01 16:01:25 +0400

    User and Category Constraints done

--- E-Commerce System ERD & Models/Model/Category.cs
+++ E-Commerce System ERD & Models/Model/Category.cs
@@ -1,15 +1,27 @@
 ﻿using System;
+using Microsoft.EntityFrameworkCore;//unique
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace E_Commerce_System_ERD___Models.Model
 {
+    [Index(nameof(categoryName), IsUnique = true)]//unique
     public class Category
     {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]//auto-generated
+        [Required]
         public int categoryId {  get; set; } // System generated 
+
+        [Required,MaxLength(50),]
         public string categoryName { get; set; } // User input
-        public string description { get; set; } // user input 
-        public string imageUrl { get; set; }//User input
+
+        [MaxLength(500)]
+        public string? description { get; set; } // user input 
+
+        [MaxLength(300)]
+        public string? imageUrl { get; set; }//User input
 
 
 
