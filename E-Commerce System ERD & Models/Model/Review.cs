0866a6c8b74b93542eac1bd8d7e0292f48b86837
Louai Al ameiri <Louai199947@gmail.com>
2026-07-01 16:23:15 +0400

    Review Constraints

--- E-Commerce System ERD & Models/Model/Review.cs
+++ E-Commerce System ERD & Models/Model/Review.cs
@@ -1,16 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace E_Commerce_System_ERD___Models.Model
 {
     public class Review
     {
+
+        [Key , DatabaseGenerated(DatabaseGeneratedOption.Identity),Required]
         public int reviewId { get; set; } // System generated 
+
+        [ForeignKey("userId"),Required]
         public int userId { get; set; } // Foreign key 
+
+        [ForeignKey("productId"),Required]
         public int productId { get; set; } // Foreign Key 
+
+        [Required,Range(1,5)]
         public int rating { get; set; } // Calculated 
-        public string comment { get; set; } // User input 
+
+        [MaxLength(1000)]
+        public string? comment { get; set; } // User input 
+
+        [Required]
         public DateTime reviewDate { get; set; } // Calculated
 
 
